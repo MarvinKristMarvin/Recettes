805964c987812206b4c0be69a8ae70e72f4aff80
MarvinKristMarvin <marvin.krist@oclock.school>
2024-08-24 19:39:58 +0200

    more style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,21 @@
 import "./Header.css";
 import logo from "/images/logo.webp";
+import { Link } from "react-router-dom";
 
 function Header() {
-	return (
-		<div className="Header">
-			<img src={logo} alt="logo" />
-			<form className="formContainer">
-				<input type="text" placeholder="Email" />
-				<input type="password" placeholder="Password" />
-				<button type="button">OK</button>
-			</form>
-		</div>
-	);
+  return (
+    <div className="Header">
+      <Link to="/accueil">
+        <img src={logo} alt="logo" />
+      </Link>
+
+      <form className="formContainer">
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="button">OK</button>
+      </form>
+    </div>
+  );
 }
 
 export default Header;
